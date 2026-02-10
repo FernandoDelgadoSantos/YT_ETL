b439ccda3a874dbb9284f611718c61c985c48f1b
Rawr <kfdelgadosantos@gmail.com>
2026-02-09 22:05:55 -0500

    Added save_to_join function, included the logs/ in the .gitignore file and save the data from the youtube API

--- video_stats.py
+++ video_stats.py
@@ -2,6 +2,7 @@ import requests
 import json 
 import os
 from dotenv import load_dotenv
+from datetime import date
 
 load_dotenv(dotenv_path="./.env")
 
@@ -21,7 +22,7 @@ def get_playlist_id():
         channel_playlistID = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
 
         # print(json.dumps(data, indent=4))
-        print(channel_playlistID)
+        # print(channel_playlistID)
         return channel_playlistID
     except requests.exceptions.RequestException as e:
         raise e 
@@ -93,10 +94,17 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e: 
         raise e 
 
+def save_to_join(extracted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+
+    with open(file_path, mode="w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
+
 if __name__ == "__main__": 
     playlistId = get_playlist_id() 
     video_ids = get_video_ids(playlistId)
-    print(extract_video_data(video_ids))
+    video_data = extract_video_data(video_ids)
+    save_to_join(video_data)
 
 
 
